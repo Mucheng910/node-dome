2653031ca7ef25990df4cd563ccdaa33a38e1edf
Muchenng910 <3544306302@qq.com>
2018-09-03 19:52:38 +0800

    updata

---
+++ globalFun.js
@@ -0,0 +1,16 @@
+// setTimeout(() => {
+//     console.log("20 seconds");
+// },2000);
+//
+//
+// var time=0;
+// var timer=setInterval(()=>{
+//     time +=2;
+//     console.log(time +"pass");
+//     if (time>=10){
+//         clearInterval(timer);
+//     }
+// },2000);
+
+console.log(__dirname); //文件路径
+console.log(__filename); //文件路径和文件名
